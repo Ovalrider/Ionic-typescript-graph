35f5446880fc80ff79d813fa26dc8423e17b2260
Ovalrider <arten.shkoren@gmail.com>
2023-04-25 11:38:25 +0300

    added graphs to servicepage

--- src/app/servicepage/servicepage.page.ts
+++ src/app/servicepage/servicepage.page.ts
@@ -1,7 +1,9 @@
 import { RecursionService } from '../service/recursion/recursion.service';
 import { SeriesService } from '../service/series/series.service';
 import { TabService } from './../service/tab/tab.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart, registerables, ChartData } from 'chart.js';
+Chart.defaults.interaction.mode = 'nearest';
 
 @Component({
   selector: 'app-servicepage',
@@ -9,19 +11,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servicepage.page.scss'],
 })
 export class ServicepagePage implements OnInit {
+  @ViewChild('lineCanvas') private lineCanvas?: ElementRef
+  lineChart: any
   xyTab = new Map()
   xyRecursion = new Map()
   xySeries = new Map()
   xyInput = new Map()
-  constructor(private tab_service : TabService, private series_service: SeriesService, private recursion_service: RecursionService) { }
+  constructor(private tab_service : TabService, private series_service: SeriesService, private recursion_service: RecursionService, ) { Chart.register(...registerables)}
 
   ngOnInit() {
+  }
+  lineChartMethod() {
+    if (this.lineCanvas instanceof Chart) {
+      this.lineCanvas.destroy();
+    }
+    this.lineChart = new Chart(this.lineCanvas?.nativeElement, {
+      type: 'line',
+      data: {
+        labels: [...this.xyTab.keys()].map(key => key.toFixed(2)),
+        datasets:[
+          {
+            label: 'Табулювання',
+            fill: false,
+            borderColor: 'rgba(75,192,192,1)'  ,
+            borderDashOffset: 0.0,
+            pointRadius: 1,
+            data: [...this.xyTab.values()].map(value => value.toFixed(4)),
+            spanGaps: false,
+          },
+          {
+            label: 'Рекурсія',
+            fill: false,
+            borderColor:  'rgba(192,75,75,1)' ,
+            borderDashOffset: 0.0,
+            pointRadius: 1,
+            data: [...this.xyRecursion.values()].map(value => value.toFixed(4)),
+            spanGaps: false,
+          },
+          {
+            label: 'Ряд',
+            fill: false,
+            borderColor: 'rgba(75,75,192,1)',
+            borderDashOffset: 0.0,
+            pointRadius: 1,
+            data: [...this.xySeries.values()].map(value => value.toFixed(4)),
+            spanGaps: false,
+          }
+        ]
+      }
+    });
   }
 
   calculate(xn:any,xk:any,h:any){
     let xn1 = parseFloat(xn)
     let xk1 = parseFloat(xk)
     let h1 = parseFloat(h)
+    if(isNaN(xn1) || isNaN(xk1) || isNaN(h1)){
+      throw new Error("Значення не числові")
+    }
+    if(xn1>=xk1){
+      throw new Error("Xn1 має бути більшим за Xk1")
+    }
     console.log("Табулювання")
     this.xyTab =this.tab_service.getTab(xn1,xk1,h1)
     console.log("Рекурсія")
@@ -41,8 +91,7 @@ export class ServicepagePage implements OnInit {
       this.xyInput.set(x.toFixed(2),s)
     
     })
-    //this.input()
-  
+    this.lineChartMethod();      
   }
 
 }
